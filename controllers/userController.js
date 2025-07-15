9fe18706d2a13fedd9fbeb9c2618cf24e529ff71
Tanbir <tanbira925@gmail.com>
2025-07-15 14:21:19 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,7 +56,7 @@ const login = async (req, res) => {
       const isMatch = await bcrypt.compare(password, existingUser.password);
       if (isMatch) {
         const userObj = {
-          firstName: existingUser.firstName,
+          firstname: existingUser.firstname,
           email: existingUser.email,
           role: existingUser.role,
         };
@@ -75,11 +75,11 @@ const login = async (req, res) => {
 };
 const register = async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body;
+    const { firstname, lastname, email, password } = req.body;
     const hashedpwd = await bcrypt.hash(password, 10);
     const user = {
-      firstName,
-      lastName,
+      firstname,
+      lastname,
       email,
       password: hashedpwd,
     };
@@ -107,11 +107,11 @@ const addUser = async (req, res) => {
 const updateProfile = async (req, res) => {
   try {
     const id = req.params.id;
-    const { firstName, lastName, email, password } = req.body;
+    const { firstname, lastname, email, password } = req.body;
     const hashedpwd = await bcrypt.hash(password, 10);
     const userObj = {
-      firstName,
-      lastName,
+      firstname,
+      lastname,
       email,
       password: hashedpwd,
     };
@@ -123,33 +123,35 @@ const updateProfile = async (req, res) => {
   }
 };
 
-const showUsers = async (req, res) => {
-  try {
-    const result = await userModel.find();
-    res.status(200).json(result);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ message: "Something went wrong" });
-  }
-};
-
 // const showUsers = async (req, res) => {
+
 //   try {
-//     const { page = 1, limit = 3, search = "" } = req.query;
-//     const skip = (page - 1) * limit;
-//     const count = await userModel.countDocuments({ firstName: { $regex: search, $options: "i" } });
-//     const total = Math.ceil(count / limit);
-//     const users = await userModel
-//       .find({ firstName: { $regex: search, $options: "i" } })
-//       .skip(skip)
-//       .limit(limit)
-//       .sort({updatedAt:-1})
-//     res.status(200).json({ users, total });
+//     const {page=1,limit=3}=req.query;
+//     const result = await userModel.find().skip(page).limit(limit);
+//     res.status(200).json(result);
 //   } catch (err) {
 //     console.log(err);
-//     res.status(500).json({ message: "Something went wrong" });
+//     res.status(400).json({ message: "Something went wrong" });
 //   }
 // };
+
+const showUsers = async (req, res) => {
+  try {
+    const { page = 1, limit = 3, search = "" } = req.query;
+    const skip = (page - 1) * limit;
+    const count = await userModel.countDocuments({ firstname: { $regex: search, $options: "i" } });
+    const total = Math.ceil(count / limit);
+    const users = await userModel
+      .find({ firstname: { $regex: search, $options: "i" } })
+      .skip(skip)
+      .limit(limit)
+      .sort({updatedAt:-1})
+    res.status(200).json({ users, total });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
 export {
   register,
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRoute.js";
 import cors from "cors";
-
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
@@ -12,17 +11,17 @@ const dbuser=encodeURIComponent(process.env.DBUSER);
 const dbpass=encodeURIComponent(process.env.DBPASS);
 
 
-mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
-   app.listen(8080, () => {
-   console.log("Server started");
- });
- });
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//    app.listen(8080, () => {
+//    console.log("Server started");
+//  });
+//  });
 
-// mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.0wl8neg.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`)
-// .then(() => {
-//   app.listen(8080, () => {
-//     console.log("Server started");
-//   });
-// });
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.0wl8neg.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`)
+.then(() => {
+  app.listen(8080, () => {
+    console.log("Server started");
+  });
+});
 
 app.use("/api/users",userRouter);
